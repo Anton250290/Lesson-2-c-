aa90204e232bf66a0e76cd0638b8ea40d1d3a393
Anton250290 <146120168+Anton250290@users.noreply.github.com>
2023-11-11 22:45:40 +0300

    homework

---
+++ 13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+ Console.WriteLine("Введите трёхзначное положительное число");
+ int num = Convert.ToInt32(Console.ReadLine());
+
+ if (num < 100 || num > 999)
+     {
+         Console.WriteLine("Третьей цифры нет");
+     }
+  else num = num % 10;
+      {
+          Console.WriteLine($"{num}");
+      }
+
+
+
